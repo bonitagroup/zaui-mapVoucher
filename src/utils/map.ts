acb25f5dde849e0966fb05202d9077da77660dbf
Văn Tú <admin@MacBook-Air-cua-Ma.local>
2025-12-16 10:18:56 +0700

    sửa open map

--- src/utils/map.ts
+++ src/utils/map.ts
@@ -1,5 +1,4 @@
 export const openMap = (lat: number, lng: number) => {
     const url = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`;
-
-    window.open(url, "_blank");
+    window.location.href = url;
 };
